85168808d7c3ae2b9e30254ddcc6bd3a813bdf2a
Chris <chriskim80@gmail.com>
2017-06-15 22:00:02 -0400

    Added smoke test

--- src/test/java/io/zipcoder/crudapp/CRUDApplicationTests.java
+++ src/test/java/io/zipcoder/crudapp/CRUDApplicationTests.java
@@ -1,16 +1,24 @@
 package io.zipcoder.crudapp;
 
+import io.zipcoder.crudapp.controller.PersonController;
 import org.junit.Test;
+import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = CRUDApplication.class)
 public class CRUDApplicationTests {
 
+	@Autowired
+	private PersonController personController;
+
+
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception{
+		assertNotNull(personController);
 	}
 
 }
