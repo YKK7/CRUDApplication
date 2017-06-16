0deaa3d2487d170c131c30716e977672a915c995
Chris <chriskim80@gmail.com>
2017-06-16 16:05:43 -0400

    Added logback-test.xml to stop the debug output on tests, moved resources folder to src/test, and finished writing first test (getAllPeopleTest)

--- src/test/java/io/zipcoder/crudapp/controller/PersonControllerTest.java
+++ src/test/java/io/zipcoder/crudapp/controller/PersonControllerTest.java
@@ -4,6 +4,7 @@ import io.zipcoder.crudapp.CRUDApplication;
 import io.zipcoder.crudapp.entity.Person;
 import io.zipcoder.crudapp.repository.PersonRepository;
 import io.zipcoder.crudapp.service.PersonService;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
@@ -11,14 +12,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = CRUDApplication.class)
+//@TestPropertySource(locations = {"classpath:application.properties", "classpath:application-h2.properties"}, properties = {"javax.persistence.sql-load-script-source=classpath:data-h2.sql", "javax.persistence.schema-generation.create-script-source=classpath:schema-h2.sql"})
 public class PersonControllerTest {
 
     @Autowired
@@ -27,23 +32,45 @@ public class PersonControllerTest {
     @Autowired
     PersonRepository personRepository;
 
+
+//    @BeforeClass
+//    public static void init(){
+//
+//    }
+
+
     @Test
     public void getAllPeopleTest(){
         //Given
-        List<Person> list = new ArrayList<Person>();
-        Person person1 = new Person("Brian Mullin", 24);
-        Person person2 = new Person("Stephen Pegram", 25);
-        list.add(person1);
-        list.add(person2);
-        ResponseEntity<List<Person>> expected = new ResponseEntity<List<Person>>(list, HttpStatus.OK);
+        String expectedName1 = "Brian Mullin";
+        String expectedName2 = "Stephen Pegram";
+        int expectedId1 = 1;
+        int expectedId2 = 2;
+        int expectedAge1 = 24;
+        int expectedAge2 = 25;
+
+        Person person1 = new Person(expectedName1, expectedAge1);
+        Person person2 = new Person(expectedName2, expectedAge2);
         personRepository.save(person1);
         personRepository.save(person2);
 
 
         //When
-        ResponseEntity<List<Person>> actual = personController.getAllPeople();
+        List<Person> list = personController.getAllPeople().getBody();
+        String actualName1 = list.get(0).getName();
+        String actualName2 = list.get(1).getName();
+        int actualAge1 = list.get(0).getAge();
+        int actualAge2 = list.get(1).getAge();
+        int actualId1 = list.get(0).getId();
+        int actualId2 = list.get(1).getId();
 
         //Then
-        assertEquals("getAllPeople should return the three Person objects that were pre-loaded into the H2 database using data-h2.sql",expected, actual);
+        assertEquals(expectedName1, actualName1);
+        assertEquals(expectedName2, actualName2);
+        assertEquals(expectedAge1, actualAge1);
+        assertEquals(expectedAge2, actualAge2);
+        assertEquals(expectedId1, actualId1);
+        assertEquals(expectedId2, actualId2);
+
     }
 }
